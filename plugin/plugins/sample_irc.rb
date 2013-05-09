bfce5aeddf406f01cb29b043c9075502887e2e6d
Nik Johnson <nik@jnstw.us>
2013-05-08 21:15:58 -0500

    Added source command

--- plugin/plugins/sample_irc.rb
+++ plugin/plugins/sample_irc.rb
@@ -2,10 +2,12 @@
 #
 class MyPlugin < Chansey::Plugin
     include Chansey::Plugin::IRCPlugin
+    SOURCE_LINK = "https://github.com/jumpandspintowin/chansey"
     events 'irc.privmsg'
 
     def init
         command("botsnack", {:priv => true}, &method(:botsnack))
+        command("source", {:priv => true}, &method(:source))
 
         command("join", {:priv => true}) do |e|
             params = e['data']['msg']['params']
@@ -31,6 +33,18 @@ class MyPlugin < Chansey::Plugin
             privmsg(event['data']['network'],
                     event['data']['msg']['nick'],
                     ":3")
+        end
+    end
+
+    def source(event)
+        if event['data']['msg']['middle'][0][0] == '#'
+            privmsg(event['data']['network'],
+                    event['data']['msg']['middle'][0],
+                    "source: #{SOURCE_LINK}")
+        else
+            privmsg(event['data']['network'],
+                    event['data']['msg']['nick'],
+                    "source: #{SOURCE_LINK}")
         end
     end
 end
