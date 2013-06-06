f855e6b92f9a157c2594ee3d4db46531339ca998
Nik Johnson <nikjohnson08@gmail.com>
2013-06-05 22:01:46 -0500

    Overhauled mc_query for the new plugin interface

--- plugin/plugins/mc_query.rb
+++ plugin/plugins/mc_query.rb
@@ -1,154 +1,5 @@
 # encoding: utf-8
 
-require 'timeout'
-
-class MCQuery < Chansey::Plugin
-    include Chansey::Plugin::IRCPlugin
-    events 'irc.privmsg'
-
-    def init
-        @command_key = '.'
-        command("mc", {:priv => true}, &method(:query_server))
-    end
-
-    def query_server(event)
-        # Get arguments
-        params = event['data']['msg']['params'].split[1..-1]
-
-        # Set reply destination
-        if event['data']['msg']['middle'].first == 'chansey' 
-            destination = event['data']['msg']['nick']
-        else
-            destination = event['data']['msg']['middle'].first
-        end
-
-        # Verify params are present
-        unless params.length.between?(1,2)
-            privmsg(event['data']['network'],
-                    destination,
-                    "Usage: <hostname> [port]")
-            return
-        end
-
-        # Do a ping query to grab most information
-        begin
-            results = mc_query(*params)
-        rescue => e
-            @log.warn "Caught#{e.exception}: #{e.backtrace.join("\n")}"
-            privmsg(event['data']['network'],
-                    event['data']['msg']['middle'][0],
-                    "Error querying server (#{e.exception})")
-            return
-        end
-
-        # If we couldn't grab ping information quit
-        if results.nil?
-            privmsg(event['data']['network'],
-                    event['data']['msg']['middle'][0],
-                    "Could not query server (maybe it's offline?)")
-            return
-        end
-
-        # Do a UT3 query to grab player list. If it fails, fail silently
-        data = {}
-        begin
-            qt3 = UT3Query.new(@log, *params) 
-            data = qt3.fullstat
-        rescue Timeout::Error
-        rescue => e
-            @log.warn "Caught#{e.exception}: #{e.backtrace.join("\n")}"
-        end
-
-        result_string = "#{params[0]}: Players: %d/%d - Version: %s - MOTD: %s" %
-            [ results[4], results[5], results[2], results[3].mc_to_irc_colors ]
-        result_string += " - Players: #{data[:players].join(', ')}" if data[:players]
-        privmsg(event['data']['network'],
-                destination,
-                result_string)
-    end
-    
-    private
-    # Uses the Server List query packet
-    def mc_query(hostname, port=25565)
-        # TCPSocket wants port as an integer
-        if port.class == String
-            port = port.to_i
-        end
-
-        # Wrap the connection in a shorter timeout
-        @log.debug "Querying #{hostname}:#{port}..."
-        s = nil
-        begin
-            Timeout::timeout(3) do
-                s = TCPSocket.new hostname, port
-            end
-        rescue Timeout::Error
-            return nil
-        end
-
-        query = [0xFE, 0x01].pack('CC')
-        s.send(query, 0)
-        data = s.readpartial(1024)
-        id, string = data.unpack('Ca*')
-        string.slice!(0..1)
-        string.force_encoding('UTF-16BE').encode!('UTF-8')
-
-        @log.debug "Query Result: #{string.split("\x00").map {|x| x.inspect }}"
-        string.split("\x00")
-    end
-end
-
-# Minecraft optionally supports the UT3 query protocol
-# This provides player information as well; We'll try it and see what happens
-class UT3Query
-    @@randgen = Random.new(1337)
-
-    def initialize(log, server, port=25565)
-        port = port.to_i if port.class != Fixnum
-        @log = log
-
-        # Create UDP Socket
-        @sock = UDPSocket.new
-        @sock.connect(server, port)
-        @id = @@randgen.bytes(4).unpack('L')[0] & 0x0F0F0F0F
-        @log.debug "Session id: #{@id} - #{[@id].pack('N').inspect}"
-        handshake
-    end
-
-    def handshake
-        packet = [0xFE, 0xFD, 0x09, @id].pack('CCCN')
-        response = nil
-        Timeout::timeout(1) do
-            @sock.send packet, 0
-            response = @sock.readpartial(1024)
-        end
-
-        _,_,@challenge = response.unpack('CLZ*')
-        @challenge = @challenge.to_i
-    end
-
-    def basicstat
-    end
-
-    def fullstat
-        request = [0xFE, 0xFD, 0x00, @id, @challenge, 0x00].pack('CCCNNN')
-        response = nil
-
-        Timeout::timeout(1) do
-            @sock.send request, 0
-            response = @sock.readpartial(4096)
-        end
-
-        response.slice!(0..15)
-        info, _, players = response.partition("\x00\x01player_\x00\x00")
-        data = Hash[*info.split("\x00")]
-        players = players.split("\x00")
-        data[:players] = players
-        @log.debug "data: #{ data.inspect }"
-        return data
-    end
-end
-
 MC_IRC_MAP = {
     "4" => "\x034",
     "c" => "\x034",
@@ -170,12 +21,231 @@ MC_IRC_MAP = {
     "r" => "\x0F\x03"
 }
 
-class String
+class ::String
     def mc_to_irc_colors
         newstr = gsub(/§(.)/) do |code|
             MC_IRC_MAP[$1]
         end
         newstr.insert(-1, "\x0F\x03")
         newstr
+    end
+end
+
+class MCQuery < Chansey::Plugin
+    include IRCPlugin
+    QUERY_TEMPLATE = "%{server}: Players: %{players}/%{max_players} - Version: %{version} - MOTD: %{motd}"
+
+    def initialize
+        irc_command("mc", {:priv => true}, &method(:query_server))
+    end
+
+    def query_server(request)
+        params = request.arg.split
+
+        # Verify params are present
+        unless params.length.between?(1,2)
+            request.privmsg("Usage: <hostname> [port]")
+            return
+        end
+
+
+        # Get basic query info
+        bool, result = mc_query(*params)
+        unless bool
+            result ||= "Could not query the minecraft server."
+            request.notice("#{params.first}: #{result}")
+            return
+        end
+
+        # Set the result string
+        result_string = QUERY_TEMPLATE % {
+            :server      => params.first,
+            :players     => result.players,
+            :max_players => result.max_players,
+            :version     => result.version,
+            :motd        => result.motd.mc_to_irc_colors
+        }
+
+        # Add player list if available
+        bool, result = ut3_query(*params)
+        if bool
+            result_string += " - Players: #{result.players.join(', ')}"
+        end
+
+        request.notice(result_string)
+    end
+    
+    private
+    # Uses the Server List query packet
+    def mc_query(hostname, port='25565')
+        mc = MinecraftPing.open(hostname, port)
+        result = wait_for_deferrable mc.query
+        mc.close_connection
+
+        result
+    end
+
+    def ut3_query(hostname, port='25565')
+        ut3 = UT3Query.open(hostname, port)
+        bool, error = wait_for_deferrable ut3.handshake
+        return bool, error unless bool
+
+        result = wait_for_deferrable ut3.fullstat
+        ut3.close_connection
+
+        result
+    end
+end
+
+# Minecraft optionally supports the UT3 query protocol
+# This provides player information as well; We'll try it and see what happens
+
+module UT3Query
+    def self.open(server, port='25565')
+        EM::open_datagram_socket('0.0.0.0', '0', Connection, server, port)
+    end
+
+    class Query
+        attr_reader :players
+        attr_reader :info
+
+        def initialize(data)
+            data.slice!(0..15)
+            info, _, players = data.partition("\x00\x01player_\x00\x00")
+            @info = Hash[*info.split("\x00")]
+            @players = players.split("\x00")
+        end
+    end
+
+    class Connection < EM::Connection
+        STATES = [ :disconnected, :handshake, :connected ]
+        RESPONSE_TIMEOUT = 5
+        @@rand = Random.new
+
+        def initialize(server, port)
+            @server = server
+            @port = port
+            @id = @@rand.bytes(4).unpack('L').first & 0x0F0F0F0F
+            @state = :disconnected
+        end
+
+        def handshake
+            d = EM::DefaultDeferrable.new
+            d.timeout(RESPONSE_TIMEOUT)
+
+            if @state != :disconnected
+                d.fail "Already connected or awaiting handshake response"
+            else
+                @state = :handshake
+                @pending_query = d
+                packet = [0xFE, 0xFD, 0x09, @id].pack('CCCN')
+                send_datagram(packet, @server, @port)
+            end
+
+            d
+        end
+
+        def fullstat
+            d = EM::DefaultDeferrable.new
+            d.timeout(RESPONSE_TIMEOUT)
+
+            if @state != :connected
+                d.fail "Not connected"
+            elsif @pending_query
+                d.fail "Query currently pending"
+            else
+                @pending_query = d
+                packet = [0xFE, 0xFD, 0x00, @id, @challenge, 0x00].pack('CCCNNN')
+                send_datagram(packet, @server, @port)
+            end
+
+            d
+        end
+
+        def receive_data(data)
+            case @state
+            when :disconnected
+                return
+            when :handshake
+                _,_,@challenge = data.unpack('CLZ*')
+                @challenge = @challenge.to_i
+                @state = :connected
+                d = @pending_query
+                @pending_query = nil
+                d.succeed
+            when :connected
+                if @pending_query
+                    q = Query.new(data)
+                    d = @pending_query
+                    @pending_query = nil
+                    d.succeed q
+                end
+            end
+        end
+
+        def unbind
+            if @pending_query
+                @pending_query.fail 'Connection suddenly closed'
+            end
+        end
+    end
+end
+
+module MinecraftPing
+    def self.open(server, port='25565')
+        EM.connect(server, port, Connection)
+    end
+
+    class Query
+        attr_reader :proto_version
+        attr_reader :version
+        attr_reader :motd
+        attr_reader :players
+        attr_reader :max_players
+
+        def initialize(data)
+            id, string = data.unpack('Ca*')
+            string.slice!(0..1)
+            string.force_encoding('UTF-16BE').encode!('UTF-8')
+            a = string.split("\x00")
+            @proto_version = a[1]
+            @version = a[2]
+            @motd = a[3]
+            @players = a[4]
+            @max_players = a[5]
+        end
+    end
+
+    class Connection < EM::Connection
+        RESPONSE_TIMEOUT = 5
+        def query
+            d = EM::DefaultDeferrable.new
+            d.timeout(RESPONSE_TIMEOUT)
+
+            if @pending_query
+                d.fail "Query already pending"
+            else
+                @pending_query = d
+                query = [0xFE, 0x01].pack('CC')
+                send_data(query)
+            end
+
+            d
+        end
+
+        def receive_data(data)
+            if d = @pending_query
+                @pending_query = nil
+                q = Query.new(data)
+                d.succeed(q)
+            end
+        end
+
+        def unbind
+            if d = @pending_query
+                @pending_query = nil
+                d.fail 'Connection suddenly closed'
+            end
+        end
     end
 end
