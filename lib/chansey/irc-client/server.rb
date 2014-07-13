0b9a7ce6ba8e17c4066cc34984ea354d87b94ae4
Nik Johnson <nik@jnstw.us>
2014-07-12 21:49:33 -0700

    Added some basic IRC protocol handling

--- lib/chansey/irc-client/server.rb
+++ lib/chansey/irc-client/server.rb
@@ -6,10 +6,17 @@ module Chansey
           @config = config
           @connection = connection
           @connection.on_message(&on_method)
+
+          @connection.send_data "NICK #{@config['nick']}"
+          @connection.send_data "USER #{@config['user']} 8 * :#{@config['fullname']}"
         end
 
         private
         def on_message(message)
+          case message[:command]
+          when :ping
+            @connection.send_data "PONG :#{message[:trailing]}"
+          end
         end
       end
     end
