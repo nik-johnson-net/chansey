e244d005c1f1f64874ce55307fa0dd52c3f5d805
Nik Johnson <nik@jnstw.us>
2014-08-10 15:11:35 -0700

    Run loop

--- lib/chansey/core.rb
+++ lib/chansey/core.rb
@@ -1,3 +1,5 @@
+require 'eventmachine'
+
 require 'chansey/simple_router'
 
 module Chansey
@@ -8,6 +10,15 @@ module Chansey
       @router = router
       @config = config
       @modules = modules
+    end
+
+    def run
+      #TODO Don't call EM.run if already in loop
+      EM.run do
+        @modules.each do |m|
+          EM.next_tick { m.post_init(self) }
+        end
+      end
     end
   end
 end
