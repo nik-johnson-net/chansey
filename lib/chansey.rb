4e1b8f6e7f2d6eb1c5b9fac8d1a1dc9ed0f18ba0
Nik Johnson <nik@jnstw.us>
2014-08-16 11:14:35 -0700

    Added documentation for module Chansey

--- lib/chansey.rb
+++ lib/chansey.rb
@@ -1,2 +1,13 @@
 require 'chansey/core'
 require 'chansey/version'
+
+# Chansey is a library on top of {http://rubyeventmachine.com EventMachine} to
+# provide a modular engine for events and handlers. A module can both generate
+# events and receive them. A router provides pub-sub functionality between the
+# two, allowing multiple modules to receive events. The original idea for
+# Chansey is to provide an easy, extensible framework for implementing an
+# internet services bot. The framework makes it simple to interconnect clients
+# for Twitter, IRC, Facebook, E-Mail, RSS, and anything else to handlers that
+# can perform any action on events.
+module Chansey
+end
