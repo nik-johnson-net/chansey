7ea3adeb80051782a81b156030294f50b8bee0b6
Nik Johnson <nik@jnstw.us>
2014-08-17 12:20:02 -0700

    Init commit of Irc client

---
+++ lib/chansey/irc/line_decoder.rb
@@ -0,0 +1,35 @@
+module Chansey
+  module Irc
+    class LineDecoder
+      def initialize
+        @buffer = ""
+      end
+
+      def map(data)
+        if data.class != String
+          raise ArgumentError
+        end
+
+        lines = []
+
+        while true
+          line, sep, data = data.partition(/\r?\n/)
+
+          if sep.empty?
+            @buffer += line
+            break
+          end
+
+          if !@buffer.empty?
+            line.prepend @buffer
+            @buffer = ""
+          end
+
+          lines << line
+        end
+
+        lines
+      end
+    end
+  end
+end
