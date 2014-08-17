ee4a9071571a6b5b08f92df7e7b0c7d6f0e26a89
Nik Johnson <nik@jnstw.us>
2014-08-17 15:14:34 -0700

    Add Irc client

---
+++ lib/chansey/modules/irc/client.rb
@@ -0,0 +1,58 @@
+require 'chansey/modules/irc/scheduled_completion'
+require 'chansey/modules/irc/handlers/autojoin'
+require 'chansey/modules/irc/handlers/ping'
+
+module Chansey
+  module Modules
+    module Irc
+      class Client < Chansey::Module
+        DEFAULT_IRC_PORT        = 6667
+        DEFAULT_RECONNECT_DELAY = 5
+
+        def initialize(core, address, port=DEFAULT_IRC_PORT, autojoin=[])
+          super(core)
+
+          @address                  = address
+          @connection               = nil
+          @next_connection_attempt  = Time.now
+          @port                     = port
+          @autojoin_channels        = autojoin
+        end
+
+        def post_init
+          connect
+        end
+
+        def send(message)
+          if @connection
+            @connection.send_data message
+
+            true
+          else
+            false
+          end
+        end
+
+        private
+        def connect 
+          handlers = [
+            Irc::Handlers::Ping.new,
+            Irc::Handlers::Autojoin.new(@autojoin_channels),
+          ]
+
+          completion = Irc::ScheduledConnectionAttempt.new(@address, @port, @next_connection_attempt, handlers)
+          completion.completion do |type, arg|
+            case type
+            when :succeeded
+              @connection = arg
+            when :failed
+              connect
+            end
+          end
+
+          @next_connection_attempt = Time.now + DEFAULT_RECONNECT_DELAY
+        end
+      end
+    end
+  end
+end
---
+++ lib/chansey/modules/irc/scheduled_connection.rb
@@ -0,0 +1,61 @@
+require 'chansey/modules/irc/connection'
+require 'chansey/modules/irc/handler'
+
+module Chansey
+  module Modules
+    module Irc
+      class ScheduledConnectionAttempt < EM::Completion
+        # Internal class for handling registration
+        class ConnectionCompletionHandler
+          include Irc::Handler
+
+          def initialize(completion)
+            @completion
+          end
+
+          def registered(connection)
+            @completion.succeed connection
+          end
+
+          def unbind
+            @completion.fail 'Connection closed'
+          end
+        end
+
+        def initialize(address, port, next_attempt, handlers)
+          super
+
+          @address  = address
+          @handlers = handlers
+          @port     = port
+
+          @handlers << ConnectionCompletionHandler.new(self)
+
+          delay = (next_attempt - Time.now).to_i
+          if delay > 0
+            EM.add_timer(delay) do
+              resolve_address
+            end
+          else
+            resolve_address
+          end
+        end
+
+        private
+        def resolve_address
+          dns_deferrable = EventMachine::DNS::Resolver.resolve(@address)
+          dns_deferrable.callback do |results|
+            ip_address = results.first
+            EM.connect(ip_address, @port, Irc::Connection, @handlers)
+          end
+
+          dns_deferrable.errback do
+            fail 'Could not resolve address'
+          end
+
+          nil
+        end
+      end
+    end
+  end
+end
